0349bd07af0d4f0d1a1986cbc1713d3c99780c07
PedroOrtega88 <pedroortegaalonso88@gmail.com>
2024-04-09 20:08:25 +0200

    -a

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
-import './App.css';
+
+import React from 'react';
+import useCustomHook from './hooks/useCustomHook';
 
 function App() {
-  const urlPokemon = 'https://pokeapi.co/api/v2/pokemon/1';
-  const urlRick = 'https://rickandmortyapi.com/api/character/1';
-  
+  const pokemonData = useCustomHook('https://pokeapi.co/api/v2/pokemon/1');
+  const rickAndMortyData = useCustomHook('https://rickandmortyapi.com/api/character/1');
+
   return (
-    <>
-    </>
+    <div>
+      <h2>Personaje Pokemon</h2>
+      {pokemonData && (
+        <div>
+          <p>{pokemonData.name}</p>
+          <img src={pokemonData.sprites.front_default} alt={pokemonData.name} />
+        </div>
+      )}
+
+      <h2>Personaje Rick and Morty</h2>
+      {rickAndMortyData && (
+        <div>
+          <p>{rickAndMortyData.name}</p>
+          <img src={rickAndMortyData.image} alt={rickAndMortyData.name} />
+        </div>
+      )}
+    </div>
   );
 }
 
